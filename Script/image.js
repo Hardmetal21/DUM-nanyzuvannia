f3eab56877b0314378026e9d58840c1b705b68d5
Hardmetal21 <133764913+Hardmetal21@users.noreply.github.com>
2025-01-14 15:17:23 +0200

    js flex

--- Script/image.js
+++ Script/image.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    let currentRotation = 0;
+
     $(".image").click(function() {
         var img = $(this); 
         var src = img.attr('src');
@@ -12,16 +14,53 @@ $(document).ready(function() {
         var finalWidth = naturalWidth * scale;
         var finalHeight = naturalHeight * scale; 
 
-        $("body").append("<div class='popup'> "+
-            "<div class='popup_bg'></div>"+
-            "<img src='" + src + "' class='popup_img' style='width:" + finalWidth + "px; height:" + finalHeight + "px;'/>"+
-            "</div>");
+        $("body").append(`
+            <div class='popup'>
+                <div class='popup_bg'></div>
+                <img src='${src}' class='popup_img' style='width:${finalWidth}px; height:${finalHeight}px;'/>
+                <div class='rotate-buttons'>
+                    <button id="rotate-left">Rotate Left</button>
+                    <button id="rotate-right">Rotate Right</button>
+                </div>
+            </div>
+        `);
+
         $(".popup").fadeIn(500).css('display', 'flex');
+
         $(".popup_bg").click(function() {
             $(".popup").fadeOut(500);
             setTimeout(function() { 
                 $(".popup").remove(); 
+                currentRotation = 0;
             }, 800);
+        });
+
+
+        function adjustSizeAfterRotation(rotation) {
+            const isRotated = (rotation / 90) % 2 !== 0; // Перевірка: чи картинка повернута на 90 або 270 градусів
+
+            // Якщо повернуто на 90 або 270 градусів, міняємо ширину та висоту місцями
+            const adjustedWidth = isRotated ? finalHeight : finalWidth;
+            const adjustedHeight = isRotated ? finalWidth : finalHeight;
+
+            $(".popup_img").css({
+                width: `${adjustedWidth}px`,
+                height: `${adjustedHeight}px`
+            });
+        }
+
+        // Функція обертання ліворуч
+        $("#rotate-left").click(function() {
+            currentRotation -= 90;
+            $(".popup_img").css("transform", `rotate(${currentRotation}deg)`);
+            adjustSizeAfterRotation(currentRotation);
+        });
+
+        // Функція обертання праворуч
+        $("#rotate-right").click(function() {
+            currentRotation += 90;
+            $(".popup_img").css("transform", `rotate(${currentRotation}deg)`);
+            adjustSizeAfterRotation(currentRotation);
         });
     });
 });
